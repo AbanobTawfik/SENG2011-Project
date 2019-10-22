27967ad109daed8ba5dc4c52496b144e69874153
Abanob Tawfik <abanob.tawfik@outlook.com>
2019-10-22 23:26:03 +1100

    created skeleton back end to be used

---
+++ VampireBackEnd/VampireBackEnd/Controllers/VampireController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VampireBackEnd.Services;
+
+namespace VampireBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VampireController : ControllerBase
+    {
+        private BloodInventoryService _bloodInventoryService;
+        public VampireController(BloodInventoryService bloodInventoryService)
+        {
+            this._bloodInventoryService = bloodInventoryService;
+        }
+        // example of GET api/Vampire
+        [HttpGet]
+        // can change routes 
+        [Route("Request")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+       
+    }
+}
---
+++ VampireBackEnd/VampireBackEnd/Services/BloodInventoryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VampireBackEnd.Services
+{
+    public class BloodInventoryService
+    {
+    }
+}
