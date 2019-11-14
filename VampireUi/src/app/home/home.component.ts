45564d71092350129ee461352eb1a0318157a53d
Abanob Tawfik <abanob.tawfik@outlook.com>
2019-11-15 10:41:54 +1100

    linked home page to backend

--- VampireUi/src/app/home/home.component.ts
+++ VampireUi/src/app/home/home.component.ts
@@ -1,78 +1,105 @@
-﻿import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { first } from 'rxjs/operators';
+﻿import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+import { first } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+import { User } from "@/_models";
+import { UserService, AuthenticationService } from "@/_services";
+import { Blood } from "../_models/Blood";
+import { environment } from "../../environments/environment";
 
-import { User } from '@/_models';
-import { UserService, AuthenticationService } from '@/_services';
-import { Blood } from '../_models/Blood';
-
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: "home.component.html" })
 export class HomeComponent implements OnInit, OnDestroy {
-    currentUser: User;
-    currentUserSubscription: Subscription;
-    users: User[] = [];
-    expiringBlood: Array<Blood>;
-    lowLevelBlood: Array<Blood>;
+  currentUser: User;
+  currentUserSubscription: Subscription;
+  users: User[] = [];
+  expiringBlood: Array<Blood>;
+  lowLevelBlood: Array<Blood>;
 
-    constructor(
-        private authenticationService: AuthenticationService,
-        private userService: UserService
-    ) {
-        this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
-            this.currentUser = user;
-        });
-    }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private http: HttpClient
+  ) {
+    this.currentUserSubscription = this.authenticationService.currentUser.subscribe(
+      user => {
+        this.currentUser = user;
+      }
+    );
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.loadAllUsers();
+    this.expiringBlood = [];
+    this.lowLevelBlood = [];
+    this.getExpiringBlood();
+    this.getLowLevelBlood();
+  }
+
+  getExpiringBlood() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "my-auth-token"
+      })
+    };
+    this.http
+      .get(environment.apiBaseUrl + "BloodInventory/GetInventory", httpOptions)
+      .subscribe(result => console.log(result));
+    // add the code to filter here
+    // you can do it all inside of the result by applying the filter function to the result
+
+    // TO DO
+    // Gets blood from Backend and fits into this.expiringBlood
+  }
+
+  getLowLevelBlood() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "my-auth-token"
+      })
+    };
+    this.http
+      .get(environment.apiBaseUrl + "BloodInventory/GetAlerts", httpOptions)
+      .subscribe(result => console.log(result));
+  }
+
+  disposeOfExpiringBlood() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "my-auth-token"
+      })
+    };
+    this.http
+      .post(
+        environment.apiBaseUrl + "BloodInventory/RemoveExpired",
+        httpOptions
+      )
+      .subscribe(result => console.log(result));
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.currentUserSubscription.unsubscribe();
+  }
+
+  deleteUser(id: number) {
+    this.userService
+      .delete(id)
+      .pipe(first())
+      .subscribe(() => {
         this.loadAllUsers();
-        this.expiringBlood = [];
-        this.lowLevelBlood = [];
-        this.getExpiringBlood();
-        this.getLowLevelBlood();
+      });
+  }
 
-    }
-
-    getExpiringBlood() {
-
-        // TO DO
-        // Gets blood from Backend and fits into this.expiringBlood
-
-    }
-
-    getLowLevelBlood() {
-
-        // TO DO
-        // Gets blood from backend and fits into this.lowLevelBlood
-
-
-    }
-
-    disposeOfExpiringBlood() {
-
-        // TO DO
-        // Send request to Backend to remove all the Blood Objects from this.expiringBlood
-        console.log("Removed Expired Blood");
-        this.getExpiringBlood();
-        this.getLowLevelBlood();
-    }
-
-    
-
-
-    ngOnDestroy() {
-        // unsubscribe to ensure no memory leaks
-        this.currentUserSubscription.unsubscribe();
-    }
-
-    deleteUser(id: number) {
-        this.userService.delete(id).pipe(first()).subscribe(() => {
-            this.loadAllUsers()
-        });
-    }
-
-    private loadAllUsers() {
-        this.userService.getAll().pipe(first()).subscribe(users => {
-            this.users = users;
-        });
-    }
+  private loadAllUsers() {
+    this.userService
+      .getAll()
+      .pipe(first())
+      .subscribe(users => {
+        this.users = users;
+      });
+  }
 }
