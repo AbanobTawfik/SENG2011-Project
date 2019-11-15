aeb4d298f61375ea01a7e3fcb3a6c87220617e96
Abanob Tawfik <abanob.tawfik@outlook.com>
2019-11-15 13:08:05 +1100

    linked request to frontend

--- VampireUi/src/app/RequestBlood/RequestBlood.component.ts
+++ VampireUi/src/app/RequestBlood/RequestBlood.component.ts
@@ -1,84 +1,87 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { Blood } from '../_models/Blood';
-import { Request } from '../_models/request';
+﻿import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+import { Blood } from "../_models/Blood";
+import { Request } from "../_models/request";
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
+import { User } from "@/_models";
+import { UserService, AuthenticationService } from "@/_services";
+import { environment } from "../../environments/environment";
 
-import { User } from '@/_models';
-import { UserService, AuthenticationService } from '@/_services';
-
-@Component({ templateUrl: 'RequestBlood.component.html' })
+@Component({ templateUrl: "RequestBlood.component.html" })
 export class RequestBloodComponent {
+  RequestBloodForm: FormGroup;
+  newRequest: Request;
+  requests: Array<Request>;
 
-	RequestBloodForm: FormGroup;
-    newRequest: Request;
-    requests: Array<Request>;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private http: HttpClient
+  ) {}
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-    ) {
+  ngOnInit() {
+    this.RequestBloodForm = this.formBuilder.group({
+      destination: ["", Validators.required],
+      amountRequired: ["", Validators.required],
+      BloodType: ["", Validators.required]
+    });
+
+    this.requests = [];
+  }
+  get f() {
+    return this.RequestBloodForm.controls;
+  }
+
+  SubmitAll() {
+    alert("Requests Submitted.");
+    console.log(this.requests[0].bloodType);
+  }
+
+  onSubmit() {
+    if (this.f.destination.value == "") {
+      alert("Destionation Required");
+    } else if (this.f.amountRequired.value == "") {
+      alert("Number of Blood Bags Required");
+    } else if (this.f.BloodType.value == "") {
+      alert("Blood Type Required");
+    } else {
+      // Add into blood Object
+      console.log("Adding");
+      console.log(this.f.amountRequired.value);
+      this.newRequest = new Request(
+        this.f.destination.value,
+        this.f.amountRequired.value,
+        this.f.BloodType.value
+      );
+
+      this.requests.push(this.newRequest);
+      console.log(this.requests.length);
+      // Do Something with it
+      const httpOptions = {
+        headers: new HttpHeaders({
+          "Content-Type": "application/json",
+          Authorization: "my-auth-token"
+        })
+      };
+      this.http
+        .post(
+          environment.apiBaseUrl + "Request/Blood",
+          this.requests,
+          httpOptions
+        )
+        .subscribe(result => console.log(result));
+      this.RequestBloodForm = this.formBuilder.group({
+        destination: ["", Validators.required],
+        amountRequired: ["", Validators.required],
+        BloodType: ["", Validators.required]
+      });
     }
 
-    ngOnInit() {
-    	this.RequestBloodForm = this.formBuilder.group({
-            destination: ['', Validators.required],
-            amountRequired: ['', Validators.required],
-            BloodType: ['', Validators.required],
-        });
-
-        this.requests = [];
-
-
-    }
-    get f() { return this.RequestBloodForm.controls; }
-
-
-    SubmitAll() {
-
-
-    	alert("Requests Submitted.");
-    	console.log(this.requests[0].bloodType);
-    }
-
-    onSubmit() {
-
-    	if (this.f.destination.value == "") {
-    		alert("Destionation Required");
-    	} else if (this.f.amountRequired.value == "") {
-    		alert("Number of Blood Bags Required");
-    	} else if (this.f.BloodType.value == "") {
-    		alert("Blood Type Required");
-    	} else {
-    		// Add into blood Object
-    		console.log("Adding");
-    		console.log(this.f.amountRequired.value);
-    		this.newRequest = new Request (this.f.destination.value, this.f.amountRequired.value, 
-    			this.f.BloodType.value);
-    		
-    		this.requests.push(this.newRequest);
-			console.log(this.requests.length);
-    		// Do Something with it
-
-    		this.RequestBloodForm = this.formBuilder.group({
-            destination: ['', Validators.required],
-            amountRequired: ['', Validators.required],
-            BloodType: ['', Validators.required],
-        });
-
-    	}
-
-
-    	
-
-    	// Send this to Rest API
-
-
-    }
-
-
-    
+    // Send this to Rest API
+  }
 }
